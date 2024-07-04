7365adc78e8e1142502297ea94f87c419a01a879
TevinDeja <teviowena@gmail.com>
2024-07-03 23:43:27 -0700

    Basic annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function
+"""
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number.
+    """
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function
+"""
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a floating-point number.
+    """
+    return str(n)
