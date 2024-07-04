0494643112dd77d0a213400bd3c303eda8a31c82
TevinDeja <teviowena@gmail.com>
2024-07-04 12:10:59 -0700

    Type Checking

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,14 +2,15 @@
 """
 type-annotated function using TypeVar
 """
+
 from typing import Mapping, TypeVar, Any, Union
 
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """Safely gets a value from a dictionary-like object
-    returning a default if the key is not found.
+    """
+    Safely gets a value from a dictionary-like object
     """
     if key in dct:
         return dct[key]
