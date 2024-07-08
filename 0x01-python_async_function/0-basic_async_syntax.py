3a623dfe9c1a184ecf390db14b7da1e051e5651a
TevinDeja <teviowena@gmail.com>
2024-07-08 03:14:12 -0700

    The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""
-Module defines an asynchronous coroutine
-"""
+"""Module defines an asynchronous coroutine"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay
+    Asynchronous coroutine that waits for random delay between 0 and max_delay
     Args:
         max_delay (int): The maximum delay in seconds. Defaults to 10.
     Returns:
